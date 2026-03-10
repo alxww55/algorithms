3d709957d5aa0a1846ef639a98c4bfe9ba6a9b54
Oleksandr Onishchenko <alxww55@icloud.com>
2026-03-10 07:01:01 +0000

    Removed useless else clause

--- recursion/recursive_sum.go
+++ recursion/recursive_sum.go
@@ -3,7 +3,6 @@ package recursion
 func RecursiveSum(slice []int) int {
 	if len(slice) == 0 {
 		return 0
-	} else {
-		return slice[0] + RecursiveSum(slice[1:])
 	}
+	return slice[0] + RecursiveSum(slice[1:])
 }
